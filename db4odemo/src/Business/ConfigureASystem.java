4aa1c4426950038e9ed7a2625beebed3d69d938b
Dhruvi zatakia <parikh.sha@husky.neu.edu>
2020-03-29 23:52:10 -0400

    food deliveryapp. id pass - sysadmin

---
+++ db4odemo/src/Business/ConfigureASystem.java
@@ -0,0 +1,34 @@
+package Business;
+
+import Business.Employee.Employee;
+import Business.Role.AdminRole;
+import Business.Role.CustomerRole;
+import Business.Role.DeliverManRole;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
+
+/**
+ *
+ * @author rrheg
+ */
+public class ConfigureASystem {
+    
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("RRH");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("Admin","sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }
+    
+}
